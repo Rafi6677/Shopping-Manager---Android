ec23bd310c347a3732da4f33f46e4c4b0411c0bd
Rafał Giemza <rafalgiemza6677@gmail.com>
2019-05-21 21:43:56 +0200

    Logging user

--- app/src/main/java/com/example/shoppingmanager/activities/registerlogin/LoginActivity.kt
+++ app/src/main/java/com/example/shoppingmanager/activities/registerlogin/LoginActivity.kt
@@ -1,8 +1,13 @@
 package com.example.shoppingmanager.activities.registerlogin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.shoppingmanager.R
+import com.example.shoppingmanager.activities.shopping.ShoppingListsActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -11,5 +16,35 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         supportActionBar?.title = "Shopping Manager - Logowanie"
+
+        login_Button.setOnClickListener {
+            performLogin()
+        }
+
+        backToRegistration_TextView.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun performLogin() {
+        val email = emailInputLogin_EditText.text.toString()
+        val password = passwordInputLogin_EditText.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Wszystkie pola muszą być wypełnione.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                val intent = Intent(this, ShoppingListsActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Podany email lub hasło są nieprawidłowe.", Toast.LENGTH_SHORT).show()
+            }
     }
 }
+
